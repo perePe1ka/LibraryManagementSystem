5bb9bf2a034013159b584dd097c0fcffffad2945
perePe1ka <vladuss_1337@list.ru>
2024-07-01 12:57:50 +0300

    some troubles with maven or jdk

--- BookService/src/main/java/ru/vladuss/bookservice/BookServiceApplication.java
+++ BookService/src/main/java/ru/vladuss/bookservice/BookServiceApplication.java
@@ -2,16 +2,18 @@ package ru.vladuss.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableJpaRepositories
+@EnableEurekaClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
-        System.setProperty("server.port", "8081");
+//        System.setProperty("server.port", "8081");
         SpringApplication.run(BookServiceApplication.class, args);
     }
 

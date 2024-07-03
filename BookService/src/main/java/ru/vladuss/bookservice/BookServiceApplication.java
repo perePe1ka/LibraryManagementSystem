b5735aa95756b450293d0b55e05fd14c9be70a82
perePe1ka <vladuss_1337@list.ru>
2024-07-03 23:26:34 +0300

    исправил ошибки, книжный сервис теперь регистрируется на сервере

--- BookService/src/main/java/ru/vladuss/bookservice/BookServiceApplication.java
+++ BookService/src/main/java/ru/vladuss/bookservice/BookServiceApplication.java
@@ -2,18 +2,19 @@ package ru.vladuss.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableJpaRepositories
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
-//        System.setProperty("server.port", "8081");
+
+
         SpringApplication.run(BookServiceApplication.class, args);
     }
 

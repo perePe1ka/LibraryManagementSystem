18399f7557b0256374df97acf313aed0d53dd09b
perePe1ka <vladuss_1337@list.ru>
2024-07-04 18:25:02 +0300

    все сервисы регаются на сервере

--- UserService/src/main/java/ru/vladuss/userservice/UserServiceApplication.java
+++ UserService/src/main/java/ru/vladuss/userservice/UserServiceApplication.java
@@ -2,14 +2,14 @@ package ru.vladuss.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableJpaAuditing
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {

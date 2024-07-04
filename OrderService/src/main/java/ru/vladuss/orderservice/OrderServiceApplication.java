1a458454b9796fdc493bf1b21e6d70e739ca4162
perePe1ka <vladuss_1337@list.ru>
2024-07-04 18:25:02 +0300

    все сервисы регаются на сервере

--- OrderService/src/main/java/ru/vladuss/orderservice/OrderServiceApplication.java
+++ OrderService/src/main/java/ru/vladuss/orderservice/OrderServiceApplication.java
@@ -2,14 +2,14 @@ package ru.vladuss.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableJpaRepositories
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
